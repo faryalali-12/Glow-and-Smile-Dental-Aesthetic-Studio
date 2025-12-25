21d60c7f3276ee19b887a39a0b2800070d3cf365
faryalali-12 <faryalali42.ma@gmail.com>
2025-12-25 09:44:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function validateForm() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+
+    if (name === "" || email === "") {
+        alert("Please fill all fields");
+        return false;
+    }
+    alert("Appointment booked successfully!");
+    return true;
+}
